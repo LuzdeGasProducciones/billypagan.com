18967772a996202b2f8ab56df1bb5cb972cf1cd1
LuzdeGasProducciones <luzdegasproducciones@gmail.com>
2026-07-11 19:19:05 +0200

    Remove button click functionality from script.js
    Removed event listener and related functionality for button click.

--- script.js
+++ script.js
@@ -1,41 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-
-const boton = document.getElementById("entrar");
-
-
-const portada = document.querySelector(".hero");
-
-
-const principal = document.querySelector(".principal");
-
-
-
-boton.addEventListener("click", function(){
-
-
-portada.style.transition="opacity 1s ease";
-
-
-portada.style.opacity="0";
-
-
-
-setTimeout(function(){
-
-
-principal.scrollIntoView({
-
-behavior:"smooth"
-
-});
-
-
-},1000);
-
-
-
-});
-
+console.log("Billy Pagán - Sitio Oficial");
 
 });
